df5e28b9a38a76eaaac20f465a40de2bde006e36
Andy <andy_wu@softmobile.com.tw>
2022-04-09 00:32:11 +0800

    better

--- getEvn.py
+++ getEvn.py
@@ -2,15 +2,15 @@ import os
 import json
 
 def getPrivateKey():
-    with open('api/pkey.json', 'r') as openfile:    
-        json_object = json.load(openfile)
     key = 'private_key'
     value = os.getenv(key)   
     print(value) 
     id_key = 'private_key_id'
     id_value = os.getenv(id_key)
     print(id_value) 
-    keyDict = {key:value,id_key:id_value}
+    keyDict = {key:value,id_key:id_value}    
+    with open('api/pkey.json', 'r') as openfile:    
+        json_object = json.load(openfile)
     json_object.update(keyDict)
     covet_json = json.dumps(json_object,indent=4)
     return covet_json
